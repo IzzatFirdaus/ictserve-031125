d6634c878a48d951bb4be54582b1cf0ceec4e711
IzzatFirdaus <2021601074@student.uitm.edu.my>
2025-11-14 19:54:05 +0800

    feat: update mail template header

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -12,7 +12,7 @@
     <td class="header" style="padding: 0 24px 24px;">
         <a href="{{ $url }}" style="display: inline-flex; align-items: center; gap: 12px;">
             @if ($brandSlot === '' || $brandSlot === 'Laravel')
-                <img src="{{ asset('images/motac-logo.png') }}" alt="{{ __('common.motac_logo') }}" width="120"
+                <img src="{{ asset('images/motac-logo.jpeg') }}" alt="{{ __('common.motac_logo') }}" width="120"
                     height="120" class="logo" loading="lazy" decoding="async">
                 <span style="font-size: 18px; font-weight: 600; color: #0f172a;">
                     {{ config('app.name', 'ICTServe') }}
