c97023af4645da775c7b8ba95e36740532ba8865
IzzatFirdaus <2021601074@student.uitm.edu.my>
2025-11-20 17:13:17 +0800

    feat(mail/jobs): update loan and helpdesk emails; queue jobs

--- app/Mail/ApprovalRequest.php
+++ app/Mail/ApprovalRequest.php
@@ -6,9 +6,9 @@ namespace App\Mail;
 
 class ApprovalRequest extends BaseMailable
 {
-    public function __construct(public LoanApplication $loanApplication) {}
+    public function __construct(public \App\Models\LoanApplication $loanApplication) {}
 
-    public function build()
+    public function build(): \Illuminate\Mail\Mailable
     {
         return $this->subject('Loan Approval Request')
             ->view('emails.loans.approval-request')
