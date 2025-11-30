42d0690b650749e9638b6ebfb96535b61030612a
IzzatFirdaus <2021601074@student.uitm.edu.my>
2025-12-01 07:40:20 +0800

    chore(app): update service provider, DevContainer, Tailwind and environment example for new integrations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 import defaultTheme from "tailwindcss/defaultTheme";
 import forms from "@tailwindcss/forms";
+import typography from "@tailwindcss/typography";
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -155,6 +156,7 @@ export default {
 
 	plugins: [
 		forms,
+		typography,
 		function ({ addUtilities }) {
 			const newUtilities = {
 				".focus-visible-only": {
